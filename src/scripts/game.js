cf966c31bdcc9357df3f97ee4c5e0ff86b20e616
Leo Chung <leo.chung97@gmail.com>
2022-05-30 11:36:48 -0400

    Game Start Logic

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -5,5 +5,19 @@ export default class Game {
   constructor() {
     this.inputs = new Input()
     this.current_word = new Dictionary()
+    this.newgame = document.querySelector("#start-button")
+    this.started = false;
+  }
+
+  addEventListeners() {
+    this.newgame.addEventListener("click", (listener) => {
+      if (!this.started) {
+        this.newgame.innerText = "Restart"
+        this.started = true;
+      } else {
+        this.newgame.innerText = "Restart"
+        this.started = true;
+      }
+    });ß
   }
 }
